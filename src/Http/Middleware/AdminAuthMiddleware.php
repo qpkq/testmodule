2c27c8194c37d31d8a7967ea28f12bea09bd6b09
qpkq <kornienkoas@stud.rosnou.ru>
2024-01-19 17:25:32 +0300

    fixes

--- src/Http/Middleware/AdminAuthMiddleware.php
+++ src/Http/Middleware/AdminAuthMiddleware.php
@@ -2,7 +2,26 @@
 
 namespace AdminDatabaseProvider\Http\Middleware;
 
+use Closure;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
 class AdminAuthMiddleware
 {
+    /**
+     * Handle an incoming request.
+     *
+     * @param Request $request
+     * @param Closure(Request): (Response|RedirectResponse) $next
+     * @return Response|RedirectResponse
+     */
+    public function handle(Request $request, Closure $next): mixed
+    {
+        if ($request->header('Authorization') !== config('app.admin_panel_token')) {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
 
+        return $next($request);
+    }
 }
