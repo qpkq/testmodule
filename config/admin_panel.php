9b2633fbd22e59ca0fa48d4c5f9ecef58a983fc6
qpkq <kornienkoas@stud.rosnou.ru>
2024-01-19 17:43:19 +0300

    fixes

---
+++ config/admin_panel.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Allowed Tables & Columns
+|--------------------------------------------------------------------------
+|
+| Tables & columns allowed for display in the admin panel.
+|
+*/
+
+return [
+    'allowed_tables' => [
+//        'users',
+    ],
+
+    'allowed_columns' => [
+//        'users' => [
+//            'id',
+//        ],
+    ],
+];
